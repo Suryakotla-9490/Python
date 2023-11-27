1a32ef5d4a7ff9e41ffa952fa8bf74eb2e9ccc3a
Surya_Kotla <121421400+Suryakotla-9490@users.noreply.github.com>
2023-11-27 16:39:46 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(nbr):
+    fact = 1
+
+
+    for itr in range(1, nbr + 1):
+        fact = fact * itr
+    return fact
+
+
+n = int(input("Enter the number to find Factorial of that number :"))
+
+print(factorial(n))
