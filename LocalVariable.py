d420f2540569100ae33be64aa3e6c591339ed39f
Surya_Kotla <121421400+Suryakotla-9490@users.noreply.github.com>
2023-12-05 14:02:20 +0530

    Add files via upload

---
+++ LocalVariable.py
@@ -0,0 +1,5 @@
+def my_function():
+    a_variable=100       # local variable created
+    print(a_variable)
+my_function()
+#sprint(a_variable) #accessing local varibale outside function causes error
